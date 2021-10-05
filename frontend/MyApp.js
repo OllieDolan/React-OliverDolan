9f00fa2fa319ec84a6a3ccb810122f3b492bbc76
OllieDolan <olliedolan10@gmail.com>
2021-10-04 17:48:24 -0700

    Add files via upload

--- frontend/MyApp.js
+++ frontend/MyApp.js
@@ -1,3 +1,5 @@
+
+import axios from 'axios';
 import React, { useState } from 'react'
 import Table from './Table'
 import Form from './Form'
@@ -19,6 +21,35 @@ function MyApp() {
   function updateList(person) {
     setCharacters([...characters, person]);
   }
+
+
+  async function fetchAll(){
+   try {
+      const response = await axios.get('http://localhost:5000/users');
+      return response.data.users_list;     
+   }
+   catch (error){
+      //We're not handling errors. Just logging into the console.
+      console.log(error); 
+      return false;         
+   }
+}
+async function makePostCall(person){
+   try {
+      const response = await axios.post('http://localhost:5000/users', person);
+      return response;
+   }
+   catch (error) {
+      console.log(error);
+      return false;
+   }
+}
+function updateList(person) { 
+   makePostCall(person).then( result => {
+   if (result)
+      setCharacters([...characters, person] );
+   });
+}
 }
 
 
